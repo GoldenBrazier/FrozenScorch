69f705f565b07916f10ad4aedd4f29879d4ef058
nimelehin <nimelehin@gmail.com>
2021-05-12 18:25:13 +0300

    Shader: Implement storage of vars

---
+++ src/Backend/Var.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <vector>
+
+#define CONSTRUCTIBLE(class_name)                                                        \
+public:                                                                                  \
+    template <class... Args>                                                             \
+    static inline std::shared_ptr<class_name> construct(Args&&... args)                  \
+    {                                                                                    \
+        return std::shared_ptr<class_name>(new class_name(std::forward<Args>(args)...)); \
+    }
+
+namespace Backend {
+
+class Var {
+    friend class Attribute;
+    friend class Uniform;
+
+public:
+    enum class Type {
+        None,
+        Attribute,
+        Uniform,
+    };
+
+    virtual ~Var() = default;
+
+    bool is_attribute() const { return type() == Type::Attribute; }
+    bool is_uniform() const { return type() == Type::Uniform; }
+
+    const std::string& name() const { return m_name; }
+    Type type() const { return m_type; }
+
+protected:
+    Var() = default;
+    Var(const std::string& name, Type type)
+        : m_type(type)
+        , m_name(name)
+    {
+    }
+
+    Var(std::string&& name, Type type)
+        : m_type(type)
+        , m_name(std::move(name))
+    {
+    }
+
+private:
+    std::string m_name {};
+    Type m_type { Type::None };
+};
+
+class Attribute : public Var {
+    CONSTRUCTIBLE(Attribute)
+
+public:
+    virtual ~Attribute() = default;
+
+    size_t index() const { return m_index; }
+
+protected:
+    Attribute() = default;
+    Attribute(const std::string& name, size_t index)
+        : Var(name, Var::Type::Attribute)
+        , m_index(index)
+    {
+    }
+
+private:
+    size_t m_index;
+};
+
+class Uniform : public Var {
+    CONSTRUCTIBLE(Uniform)
+
+public:
+    virtual ~Uniform() = default;
+
+protected:
+    Uniform() = default;
+    Uniform(const std::string& name)
+        : Var(name, Var::Type::Uniform)
+    {
+    }
+};
+
+}
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -3,52 +3,88 @@
 #include <iostream>
 #include <string>
 
-Shader::Shader(const std::string& filename)
-{
-    m_program = glCreateProgram();
-    m_shaders[0] = create_shader(load_shader(filename + ".vs"), GL_VERTEX_SHADER);
-    m_shaders[1] = create_shader(load_shader(filename + ".fs"), GL_FRAGMENT_SHADER);
+namespace Backend {
 
-    for (size_t at = 0; at < NUM_SHADERS; at++) {
-        glAttachShader(m_program, m_shaders[at]);
+Shader::Shader(const std::vector<std::string>& files, const std::vector<std::shared_ptr<Var>>& vars)
+{
+    prepare_program(files, vars);
+}
+
+void Shader::prepare_program(const std::vector<std::string>& file_paths, const std::vector<std::shared_ptr<Var>>& vars)
+{
+    m_gl_program_id = glCreateProgram();
+
+    for (auto& path : file_paths) {
+        attach_shader(path);
     }
 
-    glBindAttribLocation(m_program, 0, "position");
+    for (auto var : vars) {
+        if (var->is_attribute()) {
+            auto attribute = std::dynamic_pointer_cast<Attribute>(var);
+            glBindAttribLocation(m_gl_program_id, attribute->index(), attribute->name().c_str());
+        }
+    }
 
-    glLinkProgram(m_program);
-    check_shader_error(m_program, GL_LINK_STATUS, true, "Error linking shader program");
+    glLinkProgram(m_gl_program_id);
+    check_shader_error(m_gl_program_id, GL_LINK_STATUS, true, "Error linking shader program");
 
-    glValidateProgram(m_program);
-    check_shader_error(m_program, GL_LINK_STATUS, true, "Invalid shader program");
+    for (auto var : vars) {
+        if (var->is_uniform()) {
+            auto uniform = std::dynamic_pointer_cast<Uniform>(var);
+            register_uniform_var(uniform);
+        }
+    }
+
+    glValidateProgram(m_gl_program_id);
+    check_shader_error(m_gl_program_id, GL_LINK_STATUS, true, "Invalid shader program");
 }
 
 Shader::~Shader()
 {
-    for (size_t at = 0; at < NUM_SHADERS; at++) {
-        glDetachShader(m_program, m_shaders[at]);
-        glDeleteShader(m_shaders[at]);
+    for (auto m_gl_shader_id : m_gl_shaders_id) {
+        glDetachShader(m_gl_program_id, m_gl_shader_id);
+        glDeleteShader(m_gl_shader_id);
     }
-    glDeleteProgram(m_program);
+    glDeleteProgram(m_gl_program_id);
 }
 
-void Shader::bind()
+GLuint Shader::get_shader_type(const std::string& path)
 {
-    glUseProgram(m_program);
+    auto ends_with = [](const std::string& value, const std::string& ending) -> bool {
+        if (ending.size() > value.size())
+            return false;
+        return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
+    };
+
+    if (ends_with(path, ".vs")) {
+        return GL_VERTEX_SHADER;
+    } else if (ends_with(path, ".fs")) {
+        return GL_FRAGMENT_SHADER;
+    }
+
+    std::cerr << "Shader type is unknown: " << path << "\n";
+    std::abort();
+}
+
+void Shader::attach_shader(const std::string& path)
+{
+    m_gl_shaders_id.push_back(create_shader(load_shader(path), get_shader_type(path)));
+    glAttachShader(m_gl_program_id, m_gl_shaders_id.back());
 }
 
 GLuint Shader::create_shader(const std::string& text, GLenum shader_type)
 {
-    auto shader = glCreateShader(shader_type);
+    auto gl_shader_id = glCreateShader(shader_type);
 
     const GLchar* shaderSourceStrings[] = { text.c_str() };
     GLint shaderSourceStringLengths[1] = { static_cast<GLint>(text.size()) };
 
-    glShaderSource(shader, 1, shaderSourceStrings, shaderSourceStringLengths);
-    glCompileShader(shader);
+    glShaderSource(gl_shader_id, 1, shaderSourceStrings, shaderSourceStringLengths);
+    glCompileShader(gl_shader_id);
 
-    check_shader_error(shader, GL_COMPILE_STATUS, false, "Error: shader compilation failed: ");
+    check_shader_error(gl_shader_id, GL_COMPILE_STATUS, false, "Error: shader compilation failed: ");
 
-    return shader;
+    return gl_shader_id;
 }
 
 std::string Shader::load_shader(const std::string& filename)
@@ -83,5 +119,19 @@ void Shader::check_shader_error(GLuint shader, GLuint flag, bool program, const 
             glGetShaderInfoLog(shader, sizeof(error), NULL, error);
 
         std::cerr << errorMessage << ": '" << error << "'" << std::endl;
+        std::abort();
     }
+}
+
+void Shader::register_uniform_var(std::shared_ptr<Uniform> uniform)
+{
+    // TODO(nikita): Add check for repeating names.
+    auto m_gl_var_id = glGetUniformLocation(m_gl_program_id, uniform->name().c_str());
+    if (m_gl_var_id == 0xFFFFFFFF) {
+        std::cerr << "Can't find unifrom var";
+        std::abort();
+    }
+    m_uniform_vars[uniform->name()] = m_gl_var_id;
+}
+
 }
