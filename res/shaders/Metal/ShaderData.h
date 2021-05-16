3621ae129bea557a013d2321ea72dd6915b55947
nimelehin <nimelehin@gmail.com>
2021-05-16 22:25:29 +0300

    Backend/Metal: Use float4x4 in ShaderData

--- res/shaders/Metal/ShaderData.h
+++ res/shaders/Metal/ShaderData.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#if __METAL_MACOS__ || __METAL_IOS__
+#include <metal_stdlib>
+using namespace metal;
+#else
+#include <simd/simd.h>
+using simd::float4x4;
+#endif /* __METAL_MACOS__ || __METAL_IOS__ */
+
 struct VertUniforms {
-    float rot[16];
-    float trans[16];
+    float4x4 rot;
+    float4x4 trans;
 };
 
 struct FragmentUniforms {
