71067c4031b6fbd73d014837a044eee0216c09bb
Plunkerusr <slava.pach@gmail.com>
2021-05-20 16:55:53 +0300

    Constructors: Add constructors for Renderer,

--- src/main.cpp
+++ src/main.cpp
@@ -14,53 +14,36 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <Renderer/Renderer.h>
 
 float rotation = 0;
 float distance = 0;
 float step = 0.05;
 std::shared_ptr<Generic::Shader> shader;
 std::shared_ptr<Generic::Display> display;
-
-// TODO: Should be removed
-MTL::Buffer vertex_buffer;
-MTL::Buffer index_buffer;
+std::shared_ptr<Generic::Renderer> renderer;
+std::shared_ptr<Generic::VertexArray> vertex_array;
 
 void main_loop_callback()
 {
-    // TODO: Should move to Renderer:
-    // display.clear(0, 0.15f, 0.3f, 1.0f);
+    renderer->set_clear_color(0, 0.15f, 0.3f, 1.0f);
 
-#ifndef METAL
-    glClearColor(0, 0.15f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-#else
-    auto cmd_buffer = Ctx.metal_context()->cmd_queue().CommandBuffer();
-    auto renderpass_descriptor = Ctx.metal_context()->window().renderpass_descriptor();
-    if (!renderpass_descriptor) {
-        return;
-    }
-    renderpass_descriptor.GetColorAttachments()[0].SetClearColor({ 0, 0.15f, 0.3f, 1.0f });
-    auto rc_encoder = cmd_buffer.RenderCommandEncoder(renderpass_descriptor);
-    Ctx.metal_context()->set_render_encoder(&rc_encoder);
-#endif
+    renderer->begin();
+        renderer->clear();
 
-    shader->bind();
-    shader->set_uniform("gScale", 0.55f);
-    shader->set_uniform("gTranslation", Math::Matrix4f::Translation({ distance, distance / 2, 0 }));
-    shader->set_uniform("gRotation", Math::Matrix4f::RotationAroundZ(rotation));
+        shader->bind();
+        shader->set_uniform("gScale", 0.55f);
+        shader->set_uniform("gTranslation", Math::Matrix4f::Translation({ distance, distance / 2, 0 }));
+        shader->set_uniform("gRotation", Math::Matrix4f::RotationAroundZ(rotation));
 
-#ifndef METAL
-    mesh.draw();
-#else
-    rc_encoder.SetVertexBuffer(vertex_buffer, 0, 0);
-    rc_encoder.DrawIndexed(MTL::PrimitiveType::Triangle, 6, MTL::IndexType::UInt32, index_buffer, 0);
-    rc_encoder.EndEncoding();
-    cmd_buffer.Present(Ctx.metal_context()->window().drawable());
-    cmd_buffer.Commit();
-    cmd_buffer.WaitUntilCompleted();
-#endif
+        renderer->draw_indexed(vertex_array);
+    renderer->end();
 
     display->swap_buffers();
+
+    if (display->closed()) {
+        std::abort();
+    }
 
     distance += step;
     if (distance >= 1 || distance <= -1) {
@@ -71,19 +54,16 @@ void main_loop_callback()
     if (rotation > Math::Numbers::pi_v<float> * 2) {
         rotation = 0;
     }
-
-#ifdef METAL
-    Ctx.metal_context()->set_render_encoder(nullptr);
-#endif
 }
 
 int main(int argc, char* argv[])
 {
     display = Constructors::Display::construct(800, 600, "OpenRenderer");
+    renderer = Constructors::Renderer::construct();
 
     std::pair<std::string, int> position = { "position", 0 };
 
-    if (METAL) {
+    if (false) {
         shader = Constructors::Shader::construct(
             "res/basic.metal",
             "vert_func",
@@ -117,17 +97,10 @@ int main(int argc, char* argv[])
 
     auto main_loop = Constructors::MainLoop::construct(main_loop_callback);
 
-#ifndef METAL
-    auto va = Constructors::VertexArray::construct();
-    auto vb = va->construct_vertex_buffer(vertexes.data(), vertexes.size() * sizeof(Math::Vector3f));
-    vb->register_attribute_vec3(position.second, false, 0, 0);
-    va->construct_index_buffer(indexes.data(), indexes.size());
-
-    auto mesh = Mesh(va);
-#else
-    vertex_buffer = Ctx.metal_context()->device().NewBuffer(vertexes.data(), sizeof_data(vertexes), MTL::ResourceOptions::CpuCacheModeDefaultCache);
-    index_buffer = Ctx.metal_context()->device().NewBuffer(indexes.data(), sizeof_data(indexes), MTL::ResourceOptions::CpuCacheModeDefaultCache);
-#endif
+    vertex_array = Constructors::VertexArray::construct();
+    auto vb = vertex_array->construct_vertex_buffer(vertexes.data(), vertexes.size() * sizeof(Math::Vector3f));
+    vb->register_attribute_vec3(position.second, 0, 0);
+    vertex_array->construct_index_buffer(indexes.data(), indexes.size());
 
     // --------------------------------------------
     main_loop->run();
