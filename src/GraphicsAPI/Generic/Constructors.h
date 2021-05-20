4b77d4908dbd481f3b6b3975a8c2bf2efd0f9c6c
Plunkerusr <slava.pach@gmail.com>
2021-05-20 16:55:53 +0300

    Constructors: Add constructors for Renderer,

--- src/GraphicsAPI/Generic/Constructors.h
+++ src/GraphicsAPI/Generic/Constructors.h
@@ -6,24 +6,29 @@
 #include <GraphicsAPI/Generic/VertexArray.h>
 #include <GraphicsAPI/Generic/VertexBuffer.h>
 #include <GraphicsAPI/Metal/Display.h>
+#include <GraphicsAPI/Metal/IndexBuffer.h>
 #include <GraphicsAPI/Metal/MainLoop.h>
 #include <GraphicsAPI/Metal/Shader.h>
+#include <GraphicsAPI/Metal/VertexArray.h>
+#include <GraphicsAPI/Metal/VertexBuffer.h>
 #include <GraphicsAPI/OpenGL/Display.h>
 #include <GraphicsAPI/OpenGL/IndexBuffer.h>
 #include <GraphicsAPI/OpenGL/MainLoop.h>
 #include <GraphicsAPI/OpenGL/Shader.h>
 #include <GraphicsAPI/OpenGL/VertexArray.h>
 #include <GraphicsAPI/OpenGL/VertexBuffer.h>
+#include <Renderer/OpenGL/Renderer.h>
+#include <Renderer/Metal/Renderer.h>
 #include <cassert>
 
-#define METAL true
+// #define METAL true
 
 #define CONSTRUCTIBLE_API_DEPENDENT(class_name)                                                              \
     namespace class_name {                                                                                   \
     template <class... Args>                                                                                 \
     inline std::shared_ptr<Generic::class_name> construct(Args&&... args)                                    \
     {                                                                                                        \
-        if (METAL) {                                                                                         \
+        if (false) {                                                                                         \
             return std::shared_ptr<Generic::class_name>(new Metal::class_name(std::forward<Args>(args)...)); \
         } else {                                                                                             \
             return std::shared_ptr<Generic::class_name>(new GL::class_name(std::forward<Args>(args)...));    \
@@ -37,10 +42,9 @@ namespace Constructors {
 CONSTRUCTIBLE_API_DEPENDENT(Shader);
 CONSTRUCTIBLE_API_DEPENDENT(Display);
 CONSTRUCTIBLE_API_DEPENDENT(MainLoop);
-
-// TODO: Until Metal Impl is ready
-// CONSTRUCTIBLE_API_DEPENDENT(IndexBuffer);
-// CONSTRUCTIBLE_API_DEPENDENT(VertexBuffer);
-// CONSTRUCTIBLE_API_DEPENDENT(VertexArray);
+CONSTRUCTIBLE_API_DEPENDENT(IndexBuffer);
+CONSTRUCTIBLE_API_DEPENDENT(VertexBuffer);
+CONSTRUCTIBLE_API_DEPENDENT(VertexArray);
+CONSTRUCTIBLE_API_DEPENDENT(Renderer);
 
 }
