5bec5e986430b16a4dfd5ad4e28fa7ca802d6e9a
Plunkerusr <slava.pach@gmail.com>
2021-05-20 16:53:10 +0300

    VertexArray: Add vertex array implementation for Metal

--- src/GraphicsAPI/Generic/VertexArray.h
+++ src/GraphicsAPI/Generic/VertexArray.h
@@ -10,11 +10,10 @@ class VertexArray {
 public:
     virtual ~VertexArray() = default;
 
-    virtual void bind() = 0;
-    virtual std::shared_ptr<VertexBuffer> construct_vertex_buffer(const void* data, size_t count) = 0;
+    virtual std::shared_ptr<VertexBuffer> construct_vertex_buffer(const void* data, size_t size) = 0;
     virtual std::shared_ptr<IndexBuffer> construct_index_buffer(const void* data, size_t count) = 0;
 
-    // virtual std::shared_ptr<VertexBuffer> vertex_buffer(size_t at) = 0;
+    virtual std::shared_ptr<VertexBuffer> vertex_buffer() = 0;
     virtual std::shared_ptr<IndexBuffer> index_buffer() = 0;
 };
 
