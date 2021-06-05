cd6d4f9770a9a3b6cdb05a500f8ba6d83acd1ee9
Plunkerusr <slava.pach@gmail.com>
2021-06-05 18:51:00 +0300

    [OpenGL] Lighting: add multiple light sources and attenuation factor

--- res/basic_shader.fs
+++ res/basic_shader.fs
@@ -1,31 +1,41 @@
 #version 120
 
+#define NR_POINT_LIGHTS 4
+
 varying vec2 v_tex_coords0;
 varying vec3 world_normal;
-varying vec3 world_position_to_light_vec;
+varying vec3 world_position_to_light_vec[NR_POINT_LIGHTS];
 varying vec3 world_position_to_camera_position;
 
 uniform sampler2D g_sampler;
 uniform float g_ambient_brightness;
-uniform vec3 g_light_color;
+uniform vec3 g_light_color[NR_POINT_LIGHTS];
+uniform vec3 g_light_attenuation[NR_POINT_LIGHTS];
 
 void main()
 {
     // normalize varying vectors
     vec3 world_normal_normalized = normalize(world_normal);
-    vec3 world_position_to_light_vec_normalized = normalize(world_position_to_light_vec);
     vec3 world_position_to_camera_position_normalized = normalize(world_position_to_camera_position);
 
-    // calculate diffuse brightness
-    float diffuse_brightness = max(dot(world_normal_normalized, world_position_to_light_vec_normalized), 0);
+    vec3 all_light_color = g_ambient_brightness * vec3(1.0, 1.0, 1.0);
 
-    // calculate specular_brightness
-    vec3 world_reflected = reflect(-world_position_to_light_vec_normalized, world_normal_normalized);
-    float specular_brightness = pow(max(dot(world_position_to_camera_position_normalized, world_reflected), 0), 128);
+    for (int i = 0; i < NR_POINT_LIGHTS; i++) {
+        // calculate an attenuation
+        float distance = length(world_position_to_light_vec[i]);
+        float attenuation = 1 / (g_light_attenuation[i].x + (g_light_attenuation[i].y * distance) + (g_light_attenuation[i].z * distance * distance));
 
-    // calculate overall brightness
-    float brightness = g_ambient_brightness + diffuse_brightness + specular_brightness;
+        // calculate diffuse brightness
+        vec3 world_position_to_light_vec_normalized = normalize(world_position_to_light_vec[i]);
+        float diffuse_brightness = max(dot(world_normal_normalized, world_position_to_light_vec_normalized), 0);
+
+        // calculate specular_brightness
+        vec3 world_reflected = reflect(-world_position_to_light_vec_normalized, world_normal);
+        float specular_brightness = pow(max(dot(world_position_to_camera_position_normalized, world_reflected), 0), 128);
+
+        all_light_color += ((diffuse_brightness + specular_brightness) * g_light_color[i] * attenuation);
+    }
 
     // set output color
-	gl_FragColor = vec4(g_light_color * brightness,  1.0) * texture2D(g_sampler, v_tex_coords0.st);
+	gl_FragColor = vec4(all_light_color,  1.0) * texture2D(g_sampler, v_tex_coords0.st);
 }
--- res/basic_shader.vs
+++ res/basic_shader.vs
@@ -1,5 +1,7 @@
 #version 120
 
+#define NR_POINT_LIGHTS 4
+
 attribute vec3 position;
 attribute vec2 tex_coord;
 attribute vec3 normal;
@@ -10,24 +12,31 @@ uniform mat4 g_rotation;
 uniform mat4 g_perspective;
 uniform mat4 g_viewMatrix;
 
-uniform vec3 g_light_position;
+uniform vec3 g_light_position[NR_POINT_LIGHTS];
 uniform vec3 g_camera_position;
 
 varying vec2 v_tex_coords0;
 varying vec3 world_normal;
-varying vec3 world_position_to_light_vec;
+varying vec3 world_position_to_light_vec[NR_POINT_LIGHTS];
 varying vec3 world_position_to_camera_position;
 
 void main()
 {
+    // calculate transformation matrix for a vertex
     mat4 world_transformation = g_translation * g_rotation;
+
+    // calculate position of the vertex in the world
     vec4 world_position = world_transformation * vec4(position * g_scale, 1.0);
 
-	gl_Position = g_perspective * g_viewMatrix * world_position;
-
-	v_tex_coords0 = tex_coord;
-
+    // calculate position of the vertex normal in the world
     world_normal = (world_transformation * vec4(normal, 0.0)).xyz;
-    world_position_to_light_vec = g_light_position - world_position.xyz;
+
+    // calculate to light vector for each light
+    for (int i = 0; i < NR_POINT_LIGHTS; i++) {
+        world_position_to_light_vec[i] = g_light_position[i] - world_position.xyz;
+    }
+
+    gl_Position = g_perspective * g_viewMatrix * world_position;
     world_position_to_camera_position = g_camera_position - world_position.xyz;
+    v_tex_coords0 = tex_coord;
 }
