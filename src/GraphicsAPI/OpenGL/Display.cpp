424c4c4b03edbf094ab3d8b6fa8d66527b3c43cd
Plunkerusr <slava.pach@gmail.com>
2021-06-16 21:31:37 +0300

    OpenGL: Move to OpenGL 3.3 and GLSL 330

--- src/GraphicsAPI/OpenGL/Display.cpp
+++ src/GraphicsAPI/OpenGL/Display.cpp
@@ -20,12 +20,18 @@ Display::Display(size_t width, size_t height, const std::string& name)
     glewExperimental = GL_TRUE;
 
     SDL_Init(SDL_INIT_EVERYTHING);
+
     SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, SDL_TRUE);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
     SDL_SetRelativeMouseMode(SDL_TRUE);
 
     m_window = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
