c52bd1506f4768f58a519c16dd45b79ca8707cd7
Plunkerusr <slava.pach@gmail.com>
2021-06-19 00:39:44 +0300

    Renderer: Working with shaders in a renderer class

--- src/Renderer/Renderer.h
+++ src/Renderer/Renderer.h
@@ -1,14 +1,23 @@
 #pragma once
 
+#include <GraphicsAPI/Generic/Shader.h>
 #include <GraphicsAPI/Generic/VertexArray.h>
 #include <Model.h>
 #include <memory>
+#include <string>
+
+#include <Application/Camera.h>
 
 namespace Generic {
 
 class Renderer {
 public:
     // Scene
+    void begin_scene(const Camera& camera);
+    void end_scene();
+
+protected:
+    // GraphicsAPI specific function.
     virtual void begin() {};
     virtual void end() {};
 
@@ -19,8 +28,10 @@ public:
 
     virtual void draw_indexed(const std::shared_ptr<Generic::VertexArray>& va) = 0;
 
-    // TODO: pass here a transformation matrix and a camera so we don't deal with this in main.cpp
-    void draw_model(const Model& model);
+    void draw_model(const Model& model, const std::shared_ptr<Generic::Shader>& shader, const Math::Matrix4f& transform);
+
+private:
+    const Camera* m_camera {};
 };
 
 }
