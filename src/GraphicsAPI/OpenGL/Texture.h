dac750d8fcb7b0c56bbc7bf74ef84743626e55b5
nimelehin <nimelehin@gmail.com>
2021-06-26 18:20:42 +0300

    Editor: Print Model Component

--- src/GraphicsAPI/OpenGL/Texture.h
+++ src/GraphicsAPI/OpenGL/Texture.h
@@ -2,6 +2,7 @@
 #include <GL/glew.h>
 #include <GraphicsAPI/Generic/Texture.h>
 #include <Runtime/Image/Image.h>
+#include <Runtime/PNGLoader/PNGLoader.h>
 #include <iostream>
 #include <string>
 
@@ -9,15 +10,17 @@ namespace GL {
 
 class Texture : public Generic::Texture {
 public:
-    Texture(const Runtime::RawImage& texture_image, Generic::Texture::Types)
-        : m_texture_image(texture_image)
+    Texture(const std::string& texture_path, Generic::Texture::Types)
+        : m_texture_path(texture_path)
         , m_texture_target(GL_TEXTURE_2D)
     {
-        auto gl_format = gl_color_format(texture_image.type());
+        m_texture_image = Runtime::PNGLoader::load_rgba(m_texture_path);
+
+        auto gl_format = gl_color_format(m_texture_image.type());
         glGenTextures(1, &m_texture_object);
         glBindTexture(m_texture_target, m_texture_object);
         glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-        glTexImage2D(m_texture_target, 0, gl_format, texture_image.width(), texture_image.height(), 0, gl_format, GL_UNSIGNED_BYTE, texture_image.data<void*>());
+        glTexImage2D(m_texture_target, 0, gl_format, m_texture_image.width(), m_texture_image.height(), 0, gl_format, GL_UNSIGNED_BYTE, m_texture_image.data<void*>());
         glTexParameterf(m_texture_target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameterf(m_texture_target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     }
@@ -28,6 +31,8 @@ public:
         glActiveTexture(gl_tex_unit);
         glBindTexture(m_texture_target, m_texture_object);
     }
+
+    const std::string& path() const override { return m_texture_path; }
 
 private:
     GLenum gl_color_format(Runtime::ImageType type)
@@ -42,6 +47,7 @@ private:
     GLuint m_texture_object;
     GLenum m_texture_target;
     Runtime::RawImage m_texture_image;
+    std::string m_texture_path;
 };
 
 }
--- src/Model.h
+++ src/Model.h
@@ -10,7 +10,14 @@ public:
     explicit Model(const std::string& name);
 
     void draw() const;
+
+    Mesh& mesh() { return m_mesh; }
     const Mesh& mesh() const { return m_mesh; }
+    void set_mesh(const Mesh& mesh) { m_mesh = mesh; }
+    void set_mesh(Mesh&& mesh) { m_mesh = std::move(mesh); }
+
+    std::shared_ptr<Generic::Texture> texture() const { return m_texture; }
+    void set_texture(std::shared_ptr<Generic::Texture> texture) { m_texture = texture; }
 
 private:
     // there might be multiple textures for a model
